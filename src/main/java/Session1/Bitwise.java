dd4be99bf32d8c0e20bf636d60611ab25cfc08b7
kiran Donthuluri <kirandonthuluri@gmail.com>
2017-12-21 10:32:25 +0530

    comitting

---
+++ src/main/java/Session1/Bitwise.java
@@ -0,0 +1,15 @@
+package Session1;
+
+public class Bitwise
+{
+	    public static void main(String[] args)
+	    {
+	    	
+	    	int number1 = 12, number2 = 25, result;
+	    	
+	    	result = number1 | number2;
+	    	System.out.println(result);
+	    }
+	}
+
+
